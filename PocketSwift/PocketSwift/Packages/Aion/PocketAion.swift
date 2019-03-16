128574029d88ea4ccf130af5f005f2f373d8e7d3
Wilson Garcia <garcia.m.wilson@gmail.com>
2019-03-16 18:04:27 -0400

    feat: Creating submodules

---
+++ PocketSwift/PocketSwift/Packages/Aion/PocketAion.swift
@@ -0,0 +1,16 @@
+//
+//  PocketAion.swift
+//  PocketSwift
+//
+//  Created by Wilson Garcia on 3/16/19.
+//  Copyright © 2019 Wilson Garcia. All rights reserved.
+//
+
+import Foundation
+
+public class PocketAion: NSObject {
+    
+    public func test() {
+        print("test Aion")
+    }
+}
---
+++ PocketSwift/PocketSwift/Packages/Eth/PocketEth.swift
@@ -0,0 +1,16 @@
+//
+//  Eth.swift
+//  PocketSwift
+//
+//  Created by Wilson Garcia on 3/16/19.
+//  Copyright © 2019 Wilson Garcia. All rights reserved.
+//
+
+import Foundation
+
+public class PocketEth: NSObject {
+
+    public func test() {
+        print("test eth")
+    }
+}
