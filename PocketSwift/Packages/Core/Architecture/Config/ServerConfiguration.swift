631eda75b0c436ae578bb247fb5f020ddb46d4ca
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2019-04-13 20:18:20 -0400

    added release config only

--- PocketSwift/Packages/Core/Architecture/Config/ServerConfiguration.swift
+++ PocketSwift/Packages/Core/Architecture/Config/ServerConfiguration.swift
@@ -8,48 +8,15 @@
 
 import Foundation
 
-enum ServerConfiguration: Int {
-    case ServerURL = 0
-    case ConnectionProtocol = 1
-    case DispatchPath = 2
-    case ReportPath = 3
-    case RelayPath = 4
+enum ServerConfiguration: String {
+    case ServerURL = "dispatch.pokt.network"
+    case ConnectionProtocol = "https"
+    case DispatchPath = "/v1/dispatch"
+    case ReportPath = "/v1/report"
+    case RelayPath = "/v1/relay"
     
     func value() -> String {
-        switch self.rawValue {
-        case 0:
-            #if RELEASE
-            return "dispatch.pokt.network"
-            #else
-            return "dispatch.staging.pokt.network"
-            #endif
-        case 1:
-            #if RELEASE
-            return "https"
-            #else
-            return "https"
-            #endif
-        case 2:
-            #if RELEASE
-            return "/v1/dispatch"
-            #else
-            return "/v1/dispatch"
-            #endif
-        case 3:
-            #if RELEASE
-            return "/v1/report"
-            #else
-            return "/v1/report"
-            #endif
-        case 4:
-            #if RELEASE
-            return "/v1/relay"
-            #else
-            return "/v1/relay"
-            #endif
-        default:
-            return "Unknown error occured."
-        }
+        return self.rawValue
     }
 }
 
