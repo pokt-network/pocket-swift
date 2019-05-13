a083670887f6d0e0334d14592a2076f49b7e3cde
Wilson Garcia <garcia.m.wilson@gmail.com>
2019-05-13 07:09:47 -0400

    Adding documentation for Pocket Core

--- PocketSwift/Packages/Core/Pocket.swift
+++ PocketSwift/Packages/Core/Pocket.swift
@@ -22,6 +22,22 @@ public class Pocket: NSObject {
         }
     }
     
+    /**
+     Create new instance of Pocket.
+     - Parameters:
+        - devID : Developer Identifier
+        - network: Name of the Blockchain network that is going to be used.
+        - netIds: Arrays of Network ID
+        - maxNodes: Maximum amount of nodes
+        - requestTimeOut: Amount of time in miliseconds that the server is going to wait before returning a TimeOutException
+        - schedulerProvider: This object determines which network strategy is going to be used (Sync vs Async)
+     
+     ### Usage Example: ###
+     ````
+        Pocket(devID: "DEVID1", network: "ETH", netIds: ["4", "1"], maxNodes: 5, requestTimeOut: 1000, schedulerProvider: .main)
+     ````
+     */
+    
     init(devID: String, network: String, netIds: [String], maxNodes: Int = 5, requestTimeOut: Int = 1000, schedulerProvider: SchedulerProvider = .main){
         var blockchains: [Blockchain] = []
         netIds.forEach{ netID in
@@ -37,14 +53,64 @@ public class Pocket: NSObject {
 //        self.reportController = ReportController(with: configuration, and: schedulerProvider)
     }
     
+    /**
+     Create new instance of Pocket.
+     - Parameters:
+        - devID : Developer Identifier
+        - network: Name of the Blockchain network that is going to be used.
+        - netIds: Arrays of Network ID
+        - maxNodes: Maximum amount of nodes
+        - requestTimeOut: Amount of time in miliseconds that the server is going to wait before returning a TimeOutException
+     
+     ### Usage Example: ###
+     ````
+        Pocket(devID: "DEVID1", network: "ETH", netIds: ["4", "1"], maxNodes: 5, requestTimeOut: 1000)
+     ````
+     */
+    
     public convenience init(devID: String, network: String, netIds: [String], maxNodes: Int = 5, requestTimeOut: Int = 1000) {
         self.init(devID: devID, network: network, netIds: netIds, maxNodes: maxNodes, requestTimeOut: requestTimeOut, schedulerProvider: .main)
     }
+    
+    /**
+     Create new instance of Pocket.
+     - Parameters:
+        - devID : Developer Identifier
+        - network: Name of the Blockchain network that is going to be used.
+        - netID: Network ID
+        - maxNodes: Maximum amount of nodes
+        - requestTimeOut: Amount of time in miliseconds that the server is going to wait before returning a TimeOutException
+     
+     ### Usage Example: ###
+     ````
+        Pocket(devID: "DEVID1", network: "ETH", netID: "4", maxNodes: 5, requestTimeOut: 1000)
+     ````
+     */
     
     public convenience init(devID: String, network: String, netID: String, maxNodes: Int = 5, requestTimeOut: Int = 1000) {
         let netIds: [String] = [netID]
         self.init(devID: devID, network: network, netIds: netIds, maxNodes: maxNodes, requestTimeOut: requestTimeOut)
     }
+    
+    /**
+     Send a relay object to the server and returns an String with the response. This response depends of the structure of the Relay.
+     - Parameters:
+        - relay : Relay Object
+        - onSuccess: Callback that is going to be called if the server returns a positive response.
+        - onError: Callback that is going to be called if the server returns a negative response.
+     
+     ### Usage Example: ###
+     ````
+         let pocket = Pocket(devID: "DEVID1", network: "ETH", netID: "4", maxNodes: 5, requestTimeOut: 1000)
+         let relay = Relay.init(network: "ETH", netID: "10", data: data, devID: "DEVID1")
+         pocket.send(relay: relay, onSuccess: { response in
+            //Positive Response
+         }, onError: {error in
+            // Negative Response
+         })
+     ````
+     - SeeAlso: `Relay`
+     */
     
     public func send(relay: Relay, onSuccess: @escaping (_ data: String) ->(), onError: @escaping (_ error: Error) -> ()) {
         if !relay.isValid() {
@@ -86,18 +152,88 @@ public class Pocket: NSObject {
         }
     }
     
+    /**
+     Create a relay object and send it to the server and returns an String with the response. This response depends of the structure of the Relay.
+     - Parameters:
+        - relay : Relay Object
+        - onSuccess: Callback that is going to be called if the server returns a positive response.
+        - onError: Callback that is going to be called if the server returns a negative response.
+     
+     ### Usage Example: ###
+     ````
+         let pocket = Pocket(devID: "DEVID1", network: "ETH", netID: "4", maxNodes: 5, requestTimeOut: 1000)
+         let relay = Relay.init(network: "ETH", netID: "10", data: data, devID: "DEVID1")
+         pocket.send(relay: relay, onSuccess: { response in
+            //Positive Response
+         }, onError: {error in
+            // Negative Response
+         })
+     ````
+     - SeeAlso: `Relay`
+     */
+    
     open func send(network: String, netID: String, data: String, onSuccess: @escaping (_ data: String) ->(), onError: @escaping (_ error: Error) -> ()) {
         self.send(relay: Relay.init(network: network, netID: netID, data: data, devID: self.dispatch.configuration.devID), onSuccess: onSuccess, onError: onError)
     }
+    
+    /**
+     Adds a new Blockchain to the dispatch configuration.
+     - Parameters:
+         - network: Name of the Blockchain network that is going to be used.
+         - netID: Network ID
+     
+     ### Usage Example: ###
+     ````
+        let pocket = Pocket(devID: "DEVID1", network: "ETH", netID: "4", maxNodes: 5, requestTimeOut: 1000)
+        pocket.addBlockchain("AION", "254")
+     ````
+     */
     
     public func addBlockchain(network: String, netID: String) {
         self.dispatch.configuration.blockchains.append(Blockchain.init(network: network, netID: netID))
     }
     
     // Private interface
+    /**
+     Randomly selects a valid node
+     - Parameters:
+        - network: Name of the Blockchain network that is going to be used.
+        - netID: Network ID
+     
+     ### Usage Example: ###
+     ````
+        self.retrieveNodes(onSuccess: { (retrievedNodes) in
+            callback(Pocket.getRandomNode(nodes: retrievedNodes))
+        }) { (error) in
+            callback(nil)
+        }
+     ````
+     - SeeAlso: `Node`
+     - Returns: a valid random `Node`
+     */
     static func getRandomNode(nodes: [Node]) -> Node? {
         return nodes.isEmpty ? nil : nodes[Int.random(in: 0 ..< nodes.count)]
     }
+    
+    /**
+     Gets a random node. If the dispatch node list is empty, a new list of nodes will be obtained from Pocket backend.
+     - Parameters:
+        - network: Name of the Blockchain network that is going to be used.
+        - netID: Network ID
+        - retrieveNodes: Whether to retrieve new nodes.
+        - callback: returns a Node if exits
+     
+     ### Usage Example: ###
+     ````
+         getNode(network: relay.network, netID: relay.netID, retrieveNodes: true) {node in
+            guard let relayNode = node else {
+                onError(PocketError.nodeNotFound)
+                return
+            }
+         }
+     ````
+     - SeeAlso: `Node`
+     */
     
     func getNode(network: String, netID: String, retrieveNodes: Bool = false, callback: @escaping (_ node: Node?)->()) {
         var nodes: [Node] = []
@@ -118,6 +254,22 @@ public class Pocket: NSObject {
         }
     }
     
+    /**
+     Parses the list of service nodes from Pocket dispatch
+     - Parameters:
+        - callback: callback listener for the parse operation.
+     
+     ### Usage Example: ###
+     ````
+        self.retrieveNodes(onSuccess: { (retrievedNodes) in
+            callback(Pocket.getRandomNode(nodes: retrievedNodes))
+        }) { (error) in
+            callback(nil)
+        }
+     ````
+     - SeeAlso: `Node`
+     */
+    
     func retrieveNodes(onSuccess: @escaping (_ nodes: [Node]) ->(), onError: @escaping (_ error: Error) -> ()) {
         
         let dispatchController = DispatchController(with: self.dispatch.configuration, and: .main)
@@ -135,6 +287,27 @@ public class Pocket: NSObject {
             onError(error!)
         }
     }
+    
+    /**
+     Create a report object and send it to the server and returns an String with the response. This response depends of the structure of the Relay.
+     - Parameters:
+        - report : Report Object
+        - onSuccess: Callback that is going to be called if the server returns a positive response.
+        - onError: Callback that is going to be called if the server returns a negative response.
+     
+     ### Usage Example: ###
+     ````
+        let pocket = Pocket(devID: "DEVID1", network: "ETH", netID: "4", maxNodes: 5, requestTimeOut: 1000)
+        let report = Report.init(ip: nodes[0].ip, message: "Test please ignore")
+        pocket.send(report: report, onSuccess: { response in
+            //Positive Response
+        }, onError: {error in
+            // Negative Response
+        })
+    
+     ````
+     - SeeAlso: `Report`
+     */
     
     func send(report: Report, onSuccess: @escaping (_ data: String) ->(), onError: @escaping (_ error: Error) -> ()){
         if !report.isValid() {
